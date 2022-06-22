d52c55ae2168dc055f37d49772cb94d1710bbc0f
Werllan Gustavo <107901124+werllangustavo@users.noreply.github.com>
2022-06-22 22:19:53 +0000

    Código inicial

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
+    return render_template('base.html')
 
 app.run(host='0.0.0.0', port=81)
