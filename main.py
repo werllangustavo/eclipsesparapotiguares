76b079f33c2941b7fceb5383a77449d5de728064
Werllan Gustavo <107901124+werllangustavo@users.noreply.github.com>
2022-07-03 17:15:23 +0000

    Adicionei mais arquivos para o projeto Eclipse Para Potiguares

--- main.py
+++ main.py
@@ -5,6 +5,43 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+  return render_template('index.html')
 
+@app.route('/conceitos')
+def conceitos():
+  return render_template('conceitos.html')
+
+@app.route('/eclipsesregistrados')
+def eclipsesregistrados():
+  return render_template('eclipsesregistrados.html')
+
+@app.route('/esp26fev2017')
+def esp26fev2017():
+  return render_template('esp26022017.html')
+
+@app.route('/esp21ago2017')
+def esp21ago2017():
+  return render_template('esp21082017.html')
+
+@app.route('/elt21jan2019')
+def elt21jan2019():
+  return render_template('elt21012019.html')
+
+@app.route('/elp19nov2021')
+def elp19nov2021():
+  return render_template('elp19112021.html')
+
+@app.route('/elt16mai2022')
+def elt16mai2022():
+  return render_template('elt16052022.html')
+
+@app.route('/avisoimportante')
+def avisoimportante():
+  return render_template('avisos.html')
+
+@app.route('/avaliacoes', defaults={"estrelas": "0"})
+@app.route('/avaliacoes/<estrelas>')
+def avaliacoes(estrelas):
+  return render_template('avaliacoes.html', estrelas=estrelas)
+  
 app.run(host='0.0.0.0', port=81)
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Heroic Features v5.0.5 (https://startbootstrap.com/template/heroic-features)
+* Start Bootstrap - Full Width Pics v5.0.5 (https://startbootstrap.com/template/full-width-pics)
 * Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-heroic-features/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-full-width-pics/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
